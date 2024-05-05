f23f0741f9a6d48170d536b0cd0eebbe8382a0e5
Alexander Stepanov <alexanderlawyer97@gmail.com>
2024-05-05 17:00:59 +0400

    Initial Commit

---
+++ Project 008. User Database/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project 008. User Database
+//
+//  Created by Mac Alexander on 05.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
